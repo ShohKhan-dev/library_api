118d014af0d3e0c81c6b76fe118ab0e5d397bbab
ShohKhan-dev <shoh.hamraqulov@gmail.com>
2022-06-15 13:43:36 +0500

    Updating README.md

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Library API
 
+## Model structure:
+https://drive.google.com/file/d/1AukVC5Jewb2W2EhVueUMC8Rcp_R_D6mA/view?usp=sharing
+
 ## Sample filter:
 
 http://127.0.0.1:8000/rents/?reader=1
--- library/views.py
+++ library/views.py
@@ -8,6 +8,7 @@ from .serializers import *
 from .models import *
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAdminUser
 
 class BookList(viewsets.ModelViewSet):
 
@@ -49,6 +50,7 @@ class UserList(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
     serializer_class = UserDetailsSerializer
+    # permission_classes = [IsAdminUser]
 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['id', 'username']
